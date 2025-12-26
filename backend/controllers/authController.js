a977928f436ee0ceb9ca016b6bdf0797f6c5e465
Ekram Asrar <ekramasrar94@gmail.com>
2025-12-26 03:11:56 -0800

    feat: Backend - Implement user registration and login logic

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,7 +1,81 @@
-exports.register = (req, res) => {
-    res.status(200).json({ message: 'Register route placeholder' });
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const generateToken = (id, organizationId) => {
+    return jwt.sign({
+        id, organizationId: organizationId
+    }, process.env.JWT_SECRET, { expiresIn: '1h' });
+};
+
+
+exports.register = async (req, res) => {
+    const { name, email, password } = req.body;
+
+    try {
+        let user = await User.findOne({ email });
+
+        if (user) {
+            return res.status(400).json({ message: 'User already exists' });
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(password, salt);
+
+        user = new User({
+            name,
+            email,
+            password: hashPassword
+        });
+
+        await user.save();
+
+        const token = generateToken(user._id, user.organization);
+
+        res.status(201).json({
+            message: 'User registered successfully',
+            token,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                organization: user.organization,
+                role: user.role
+            }
+        });
+    } catch (error) {
+        console.error('Registration error:', error);
+        res.status(500).json({ message: 'Server error' });
+    }
 }
 
-exports.login = (req, res) => {
-    res.status(200).json({ message: 'Login route placeholder' });
-}
+exports.login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email }).select('+password');
+        if (!user) {
+            return res.status(400).json({ message: 'Invalid creadentials' });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Invalid creadentials' });
+        }
+
+        const token = generateToken(user._id, user.organization);
+        res.status(200).json({
+            message: 'Logged in successfully',
+            token,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                organization: user.organization,
+                role: user.role
+            }
+        });
+    } catch (error) {
+        console.error('Login error:', error);
+        res.status(500).json({ message: 'Server error' });
+    }
+};
