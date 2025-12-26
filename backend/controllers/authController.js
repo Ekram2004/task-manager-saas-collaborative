50c44bf50fe210b6f5cc4e6d33028b13c7288371
Ekram Asrar <ekramasrar94@gmail.com>
2025-12-26 00:38:05 -0800

    feat: Backend - User model, auth routes, and controller placeholders

---
+++ backend/controllers/authController.js
@@ -0,0 +1,7 @@
+exports.register = (req, res) => {
+    res.status(200).json({ message: 'Register route placeholder' });
+}
+
+exports.login = (req, res) => {
+    res.status(200).json({ message: 'Login route placeholder' });
+}
